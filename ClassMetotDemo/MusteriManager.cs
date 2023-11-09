0c60884cba3b0c6557b772f610f0401350a67c19
cgrzmustafa <cigersizmustafa5@gmail.com>
2023-11-09 17:55:29 +0300

    2.11.2023 ders sonu

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler. Müşteri eklendi : " + musteri.Adi);
+        }
+
+        public void MusteriListele(Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler. Müşteri listelendi : " + musteri.Adi);
+        }
+        public void MusteriSil(Musteri musteri)
+        {
+            Console.WriteLine("Tebrikler. Müşteri silindi : " + musteri.Adi);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Id = 1;
+            musteri1.Adi = "Mustafa";
+            musteri1.Soyadi = "Ciğersiz";
+            musteri1.Tc = "12345678910";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Id = 2;
+            musteri2.Adi = "Hakan";
+            musteri2.Soyadi = "Doğan";
+            musteri2.Tc = "12345678911";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Id = 3;
+            musteri3.Adi = "Can";
+            musteri3.Soyadi = "Yılmaz";
+            musteri3.Tc = "12345678912";
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            foreach (var musteri in musteriler)
+            {
+                Console.WriteLine("ID: " + musteri.Id);
+                Console.WriteLine("Adı: " + musteri.Adi);
+                Console.WriteLine("Soyadı: " + musteri.Soyadi);
+                Console.WriteLine("TC: " + musteri1.Tc);
+                Console.WriteLine("-------------------------");
+            }
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriEkle(musteri2);
+            musteriManager.MusteriEkle(musteri3);
+        }
+    }
+}
